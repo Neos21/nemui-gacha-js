2e99c8c623be8b4fcf427bb5f64fa875e8d65762
Neos21 <neos21@gmail.com>
2020-04-19 00:01:11 +0900

    Updates

---
+++ precache-manifest.68c2afbf6eb0a43c013a314ef847b212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeadbc4babf3a79be05ee97a9c8f0d38",
+    "url": "/nemui-gacha-js/index.html"
+  },
+  {
+    "revision": "d041bc2cc46bd95aef2b",
+    "url": "/nemui-gacha-js/static/css/main.fb7067c3.chunk.css"
+  },
+  {
+    "revision": "3fa386d02f23ecf75e5c",
+    "url": "/nemui-gacha-js/static/js/2.74999597.chunk.js"
+  },
+  {
+    "revision": "505abae059ca37ca797649d03e00a588",
+    "url": "/nemui-gacha-js/static/js/2.74999597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d041bc2cc46bd95aef2b",
+    "url": "/nemui-gacha-js/static/js/main.86027a54.chunk.js"
+  },
+  {
+    "revision": "b0684a11fbd16d0893f8",
+    "url": "/nemui-gacha-js/static/js/runtime-main.35a5399f.js"
+  }
+]);
